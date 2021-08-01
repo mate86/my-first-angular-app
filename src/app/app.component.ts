990d91119f12ceb198e1630e7e720052d1e9907a
mate86 <balazsm86@gmail.com>
2021-08-01 14:33:31 +0200

    Getting the Index when using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,8 @@ export class AppComponent {
 
   onTogglDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
 
