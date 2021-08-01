6628d9deba57aaf18d04f0cb31342e9838cfbc28
mate86 <balazsm86@gmail.com>
2021-08-01 12:22:50 +0200

    Assignment 3: Practicing Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-angular-app';
+  showSecret = false;
+  log = [];
+
+
+  onTogglDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
+
